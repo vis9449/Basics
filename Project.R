3292f2991164acb949b4280c7f6317d8ed02a9db
Vishal Lolam <68023568+vis9449@users.noreply.github.com>
2020-07-09 12:49:52 +0530

    Add files via upload

---
+++ Project.R
@@ -0,0 +1,25 @@
+2+3
+
+base <- 5
+height <- 12
+
+area <- base * height
+area
+
+TRUE
+class(T)
+FALSE
+class(F)
+
+is.numeric(2)
+is.numeric(2L)
+is.integer(2)
+is.integer(2L)
+
+"I love data science"
+class("I love data science")
+
+as.numeric(4.560)
+as.integer(4.560)
+
+as.integer(3.14)
