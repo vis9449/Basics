c66c86f9e8350daee2562cf1548c1c178016807f
Vishal Lolam <68023568+vis9449@users.noreply.github.com>
2020-07-09 13:14:22 +0530

    Final

--- Initial functions.R
+++ Initial functions.R
@@ -1,6 +1,31 @@
 <<<<<<< HEAD
+2+3
+
+base <- 5
+height <- 12
+
+area <- base * height
+area
+
+TRUE
+class(T)
+FALSE
+class(F)
+
+is.numeric(2)
+is.numeric(2L)
+is.integer(2)
+is.integer(2L)
+
+"I love data science"
+class("I love data science")
+
+as.numeric(4.560)
+as.integer(4.560)
+
+==========================================================================================================
   
- #create vectors C()
+#create vectors C()
 food <- c("burger", "pizza", "fries", "juice", "noodles")
 quantity <- c(2, 5, 6, 9, 2)
 
